444ff6e7fa47547bd70f012e6170698bea5d21d1
Jim Cummins <jim.for.cy@gmail.com>
2013-10-24 20:03:00 -0500

    Moved datetime helpers from controller.

--- app/controllers/RacesController.php
+++ app/controllers/RacesController.php
@@ -56,33 +56,7 @@ class RacesController extends BaseController {
 		$race = $this->race->where('slug',$slug)->first();
 
 
-		/**
-		* Convert datetime and timezone from UTC to Local string, datetime, and offset
-		*
-		*	@param string $utcDateTimeString
-		*	@param string $timezoneString
-		*	@return Converted Localized Array
-		*/
-		function utcToLocal($utcDateTimeString, $timezoneString) {
-			$dzt = new DateTimeZone($timezoneString);
-			$utc = new DateTimeZone('UTC');
-			$dt = new DateTime($utcDateTimeString,$utc);
-			$dt->setTimezone($dzt);
-
-			return array(
-			'fullstring' => $dt->format('Y-m-d h:ia') .' GMT' . number_format($dzt->getOffset($dt)/3600,1),
-			'offset' => $dzt->getOffset($dt),
-			 'datetime' => $dt
-			 );
-		}
-
-		function localToUtc($localDateTimeString, $timezoneString) {
-			$dzt = new DateTimeZone($timezoneString);
-			$utc = new DateTimeZone('UTC');
-			$dt = new DateTime($localDateTimeString, $dzt);
-			$dt->setTimezone($utc);
-			return $dt;
-		}
+		
 
 		dd(
 			array(
---
+++ app/support/helpers.php
@@ -0,0 +1,34 @@
+<?php
+if ( ! function_exists('utcToLocal'))
+{
+	/**
+	* Convert datetime and timezone from UTC to Local string, datetime, and offset
+	*
+	*	@param string $utcDateTimeString
+	*	@param string $timezoneString
+	*	@return Converted Localized Array
+	*/
+	function utcToLocal($utcDateTimeString, $timezoneString) {
+		$dzt = new DateTimeZone($timezoneString);
+		$utc = new DateTimeZone('UTC');
+		$dt = new DateTime($utcDateTimeString,$utc);
+		$dt->setTimezone($dzt);
+
+		return array(
+		'fullstring' => $dt->format('Y-m-d h:ia') .' GMT' . number_format($dzt->getOffset($dt)/3600,1),
+		'offset' => $dzt->getOffset($dt),
+		 'datetime' => $dt
+		 );
+	}
+}
+
+if ( ! function_exists('localToUtc'))
+{
+	function localToUtc($localDateTimeString, $timezoneString) {
+		$dzt = new DateTimeZone($timezoneString);
+		$utc = new DateTimeZone('UTC');
+		$dt = new DateTime($localDateTimeString, $dzt);
+		$dt->setTimezone($utc);
+		return $dt;
+	}
+}
