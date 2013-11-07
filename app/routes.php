3171a7572e78fec90831a972468ef6f838d43e90
Jim Cummins <jim.for.cy@gmail.com>
2013-11-06 19:15:21 -0600

    Test omnipay stripe support.

--- app/routes.php
+++ app/routes.php
@@ -23,3 +23,25 @@ Route::resource('groups', 'GroupController');
 
 Route::resource('races', 'RacesController');
 
+use Omnipay\Common\GatewayFactory;
+
+Route::get('donate', function(){
+
+	$gateway = GatewayFactory::create('Stripe');
+	$gateway->setApiKey(Config::get('app.stripe.apikey',''));
+
+	$formData = ['number' => '4242424242424242', 'expiryMonth' => '6', 'expiryYear' => '2016', 'cvv' => '123'];
+	$response = $gateway->purchase(['amount' => '10.00', 'currency' => 'USD', 'card' => $formData])->send();
+
+	if ($response->isSuccessful()) {
+	    // payment was successful: update database
+	    dd($response);
+	} elseif ($response->isRedirect()) {
+	    // redirect to offsite payment gateway
+	    $response->redirect();
+	} else {
+	    // payment failed: display message to customer
+	    dd($response->getMessage());
+	}
+
+});
